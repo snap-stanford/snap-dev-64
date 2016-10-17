a45d82a056750c4d92997b463d86acc1a43b130e
mohanaprasad1994 <mohanaprasad1994@yahoo.co.in>
2016-10-16 17:08:54 -0700

    changed Snap.h and Snap.cpp to not include a lot of files so that the code compiles

--- snap-core/Snap.cpp
+++ snap-core/Snap.cpp
@@ -17,7 +17,7 @@
 //#include "mmgraph.cpp"       // multimodal graphs
 #include "network.cpp"       // networks
 #include "networkmp.cpp"     // networks OMP
-#include "timenet.cpp"       // time evolving networks
+//#include "timenet.cpp"       // time evolving networks              Need to uncomment this
 #include "mmnet.cpp"         // multimodal networks
 
 // algorithms
@@ -26,25 +26,25 @@
 #include "cncom.cpp"         // connected components
 #include "alg.cpp"           // misc graph algorithms
 #include "gsvd.cpp"          // SVD and eigenvector computations
-#include "gstat.cpp"         // graph statistics
-#include "centr.cpp"         // centrality measures
-#include "cmty.cpp"          // community detection algorithms
+//#include "gstat.cpp"         // graph statistics        Need to uncomment this
+//#include "centr.cpp"         // centrality measures    Need to uncomment this
+//#include "cmty.cpp"          // community detection algorithms   Need to uncomment this
 #include "flow.cpp"          // network flow algorithms
-#include "coreper.cpp"       // core-periphery algorithms
-#include "triad.cpp"         // clustering coefficient and triads
+//#include "coreper.cpp"       // core-periphery algorithms        Need to uncomment this
+//#include "triad.cpp"         // clustering coefficient and triads Need to uncomment this
 //#include "casc.cpp"          // cascade detection^M
 
 // graph generators
-#include "ggen.cpp"          // graph generators (preferential attachment, small-world, ...)
-#include "ff.cpp"            // forest fire graph generator
+//#include "ggen.cpp"          // graph generators (preferential attachment, small-world, ...)       Need to uncomment this
+//#include "ff.cpp"            // forest fire graph generator           Need to uncomment this
 
 #include "gviz.cpp"
 #include "ghash.cpp"
-#include "statplot.cpp"
+//#include "statplot.cpp"            Need to uncomment this
 #include "gio.cpp"
 
 // table data structures and algorithms
-#include "table.cpp"         // table
+//#include "table.cpp"         // table     Need to uncomment this
 //#include "conv.cpp"
 #include "numpy.cpp"         // numpy conversion
 
--- snap-core/Snap.h
+++ snap-core/Snap.h
@@ -20,36 +20,36 @@
 #include "network.h"         // networks
 #include "networkmp.h"       // networks OMP
 #include "bignet.h"          // large networks
-#include "timenet.h"         // time evolving networks
+//#include "timenet.h"         // time evolving networks             Need to uncomment this
 #include "mmnet.h"           // multimodal networks
 
 // algorithms
 #include "subgraph.h"        // subgraph manipulations
 #include "anf.h"             // approximate diameter calculation
-#include "bfsdfs.h"          // breadth and depth first search
+//#include "bfsdfs.h"          // breadth and depth first search      Need to uncomment this
 #include "cncom.h"           // connected components
 #include "kcore.h"           // k-core decomposition
 #include "alg.h"             // misc graph algorithms
-#include "triad.h"           // clustering coefficient and triads
+//#include "triad.h"           // clustering coefficient and triads   Need to uncomment this
 #include "gsvd.h"            // SVD and eigenvector computations
-#include "gstat.h"           // graph statistics
-#include "centr.h"           // centrality measures
-#include "cmty.h"            // community detection algorithms
+//#include "gstat.h"           // graph statistics                      Need to uncomment this
+//#include "centr.h"           // centrality measures                   Need to uncomment this
+//#include "cmty.h"            // community detection algorithms        Need to uncomment this
 #include "flow.h"            // network flow algorithms
-#include "coreper.h"         // core-periphery algorithms
+//#include "coreper.h"         // core-periphery algorithms             Need to uncomment this
 #include "randwalk.h"        // Personalized PageRank
 
 // graph generators
-#include "ggen.h"            // graph generators (preferential attachment, small-world, ...)
-#include "ff.h"              // forest fire graph generator
+//#include "ggen.h"            // graph generators (preferential attachment, small-world, ...)       Need to uncomment this
+//#include "ff.h"              // forest fire graph generator            Need to uncomment this
 
 #include "gio.h"
 #include "gviz.h"
 #include "ghash.h"
-#include "statplot.h"
+//#include "statplot.h"                                                 Need to uncomment this
 
 // table data structures and algorithms
-#include "table.h"         // table
+//#include "table.h"         // table                    Need to uncomment this
 //#include "conv.h" 	   // conversion functions - table to graph
 #include "numpy.h" 	   // numpy conversion
 //#include "casc.h"          // cascade detection^M
