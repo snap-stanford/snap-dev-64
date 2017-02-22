67bf5f13140ed234fb92f7709b242cca1d3ea94b
mohanaprasad1994 <mohanaprasad1994@yahoo.co.in>
2017-02-22 00:10:56 -0800

    separated 64 bit and 32 bit tests for THash, TUNGraph and TNGraph

---
+++ test/test-THash64.cpp
@@ -0,0 +1,525 @@
+#include <gtest/gtest.h>
+#include "Snap.h"
+#include "iostream"
+
+int64 Prime64(const int64& n);
+bool IsPrime64(const int64& d);
+// Test the default constructor
+TEST(TIntInt64H, DefaultConstructor) {
+  TIntInt64H TableInt;
+
+  EXPECT_EQ(1,TableInt.Empty());
+  EXPECT_EQ(0,TableInt.Len());
+  EXPECT_EQ(0,TableInt.GetMxKeyIds());
+}
+
+// Table manipulations and inserting keys and data >32bits
+TEST(TIntInt64H, ManipulateTableWithInt64KeysAndValues) {
+  const int64 OFFSET = 100000000000000;
+  const int64 NElems = 1000;
+  int64 DDist = 10;
+  const char *FName = "test.hashint64.dat";
+  TIntInt64H TableInt;
+  TIntInt64H TableInt1;
+  TIntInt64H TableInt2;
+  int64 i;
+  int64 d;
+  int64 n;
+  int64 Id;
+  int64 Key;
+  int64 KeySumVal;
+  int64 DatSumVal;
+  int64 KeySum;
+  int64 DatSum;
+  int64 KeySumDel;
+  int64 DatSumDel;
+  int64 DelCount;
+  int64 Count;
+
+  // add table elements
+  d = Prime64(NElems);
+  n = d + OFFSET;
+  KeySumVal = 0;
+  DatSumVal = 0;
+  for (i = 0; i < NElems; i++) {
+    TableInt.AddDat(n,n+1);
+    KeySumVal += n;
+    DatSumVal += (n+1);
+    //printf("add %d %d\n", n, n+1);
+    //n = (n + d) % NElems;
+    n = n+ d;
+  }
+  EXPECT_EQ(0,TableInt.Empty());
+  EXPECT_EQ(NElems,TableInt.Len());
+
+  // verify elements by successive keys
+  KeySum = 0;
+  DatSum = 0;
+  n = d + OFFSET;
+  //std::cout<< OFFSET<< " "<<n<<std::endl;
+  for (i = 0; i < NElems; i++) {
+    Id = TableInt.GetKeyId(n);
+    EXPECT_EQ(1,Id >= 0);
+    Key = TableInt.GetKey(Id);
+    EXPECT_EQ(0,TableInt.GetDat(Key)-Key-1);
+    KeySum += Key;
+    DatSum += TableInt.GetDat(Key);
+    //std::cout <<n <<" "<<Key<<" "<< TableInt.GetDat(Key)<<std::endl;
+    n = n + d;
+  }
+
+  EXPECT_EQ(0,KeySumVal - KeySum);
+  EXPECT_EQ(0,DatSumVal - DatSum);
+
+//  // verify elements by distant keys
+//    KeySum = 0;
+//    DatSum = 0;
+//    n = d + OFFSET;
+//    for (i = 0; i < NElems; i++) {
+//      Id = TableInt.GetKeyId(n);
+//      EXPECT_EQ(1,Id >= 0);
+//      Key = TableInt.GetKey(Id);
+//      EXPECT_EQ(0,TableInt.GetDat(Key)-Key-1);
+//      KeySum += Key;
+//      DatSum += TableInt.GetDat(Key);
+//      n = (n + d) % NElems;
+//    }
+//
+//    EXPECT_EQ(0,KeySumVal - KeySum);
+//    EXPECT_EQ(0,DatSumVal - DatSum);
+
+    // verify elements by iterator
+    KeySum = 0;
+    DatSum = 0;
+    for (TIntInt64H::TIter It = TableInt.BegI(); It < TableInt.EndI(); It++) {
+      EXPECT_EQ(0,It.GetDat()-It.GetKey()-1);
+      KeySum += It.GetKey();
+      DatSum += It.GetDat();
+    }
+
+    EXPECT_EQ(0,KeySumVal - KeySum);
+    EXPECT_EQ(0,DatSumVal - DatSum);
+
+    // verify elements by key index
+    KeySum = 0;
+    DatSum = 0;
+    Id = TableInt.FFirstKeyId();
+    while (TableInt.FNextKeyId(Id)) {
+      EXPECT_EQ(1,Id >= 0);
+      Key = TableInt.GetKey(Id);
+      EXPECT_EQ(0,TableInt.GetDat(Key)-Key-1);
+      KeySum += Key;
+      DatSum += TableInt.GetDat(Key);
+    }
+
+    EXPECT_EQ(0,KeySumVal - KeySum);
+    EXPECT_EQ(0,DatSumVal - DatSum);
+
+    // delete elements
+    DelCount = 0;
+    KeySumDel = 0;
+    DatSumDel = 0;
+    n = d + OFFSET;
+    for (n = d + OFFSET; n < d + OFFSET + NElems*d; n += DDist*d) {
+      Id = TableInt.GetKeyId(n);
+      //printf("del %d %d %d\n", n, Id, (int) TableInt[Id]);
+      KeySumDel += n;
+      DatSumDel += TableInt[Id];
+      TableInt.DelKeyId(Id);
+      DelCount++;
+    }
+    EXPECT_EQ(0,TableInt.Empty());
+    EXPECT_EQ(NElems-DelCount,TableInt.Len());
+
+    // verify elements by iterator
+    KeySum = 0;
+    DatSum = 0;
+    Count = 0;
+    for (TIntInt64H::TIter It = TableInt.BegI(); It < TableInt.EndI(); It++) {
+      EXPECT_EQ(0,It.GetDat()-It.GetKey()-1);
+      //printf("get %d %d\n", (int) It.GetKey(), (int) It.GetDat());
+      KeySum += It.GetKey();
+      DatSum += It.GetDat();
+      Count++;
+    }
+
+    EXPECT_EQ(NElems-DelCount,Count);
+    EXPECT_EQ(0,KeySumVal - KeySumDel - KeySum);
+    EXPECT_EQ(0,DatSumVal - DatSumDel - DatSum);
+
+    // assignment
+    TableInt1 = TableInt;
+    EXPECT_EQ(0,TableInt1.Empty());
+    EXPECT_EQ(NElems-DelCount,TableInt1.Len());
+
+    // verify elements by iterator
+    KeySum = 0;
+    DatSum = 0;
+    Count = 0;
+    for (TIntInt64H::TIter It = TableInt1.BegI(); It < TableInt1.EndI(); It++) {
+      EXPECT_EQ(0,It.GetDat()-It.GetKey()-1);
+      //printf("get %d %d\n", (int) It.GetKey(), (int) It.GetDat());
+      KeySum += It.GetKey();
+      DatSum += It.GetDat();
+      Count++;
+    }
+
+    EXPECT_EQ(NElems-DelCount,Count);
+    EXPECT_EQ(0,KeySumVal - KeySumDel - KeySum);
+    EXPECT_EQ(0,DatSumVal - DatSumDel - DatSum);
+
+    // saving and loading
+    {
+      TFOut FOut(FName);
+      TableInt.Save(FOut);
+      FOut.Flush();
+    }
+
+    {
+      TFIn FIn(FName);
+      TableInt2.Load(FIn);
+    }
+
+    EXPECT_EQ(NElems-DelCount,TableInt2.Len());
+
+    // verify elements by iterator
+    KeySum = 0;
+    DatSum = 0;
+    Count = 0;
+    for (TIntInt64H::TIter It = TableInt2.BegI(); It < TableInt2.EndI(); It++) {
+      EXPECT_EQ(0,It.GetDat()-It.GetKey()-1);
+      //printf("get %d %d\n", (int) It.GetKey(), (int) It.GetDat());
+      KeySum += It.GetKey();
+      DatSum += It.GetDat();
+      Count++;
+    }
+
+    EXPECT_EQ(NElems-DelCount,Count);
+    EXPECT_EQ(0,KeySumVal - KeySumDel - KeySum);
+    EXPECT_EQ(0,DatSumVal - DatSumDel - DatSum);
+
+    // remove all elements
+    for (i = 0; i < Count; i++) {
+      Id = TableInt.GetRndKeyId(TInt::Rnd, 0.5);
+      TableInt.DelKeyId(Id);
+    }
+    EXPECT_EQ(0,TableInt.Len());
+    EXPECT_EQ(1,TableInt.Empty());
+
+    // verify elements by iterator
+    KeySum = 0;
+    DatSum = 0;
+    Count = 0;
+    for (TIntInt64H::TIter It = TableInt.BegI(); It < TableInt.EndI(); It++) {
+      EXPECT_EQ(0,It.GetDat()-It.GetKey()-1);
+      //printf("get %d %d\n", (int) It.GetKey(), (int) It.GetDat());
+      KeySum += It.GetKey();
+      DatSum += It.GetDat();
+      Count++;
+    }
+
+    EXPECT_EQ(0,Count);
+    EXPECT_EQ(0,KeySum);
+    EXPECT_EQ(0,DatSum);
+
+    // clear the table
+    TableInt1.Clr();
+    EXPECT_EQ(0,TableInt1.Len());
+    EXPECT_EQ(1,TableInt1.Empty());
+
+    // verify elements by iterator
+    KeySum = 0;
+    DatSum = 0;
+    Count = 0;
+    for (TIntInt64H::TIter It = TableInt1.BegI(); It < TableInt1.EndI(); It++) {
+      EXPECT_EQ(0,It.GetDat()-It.GetKey()-1);
+      //printf("get %d %d\n", (int) It.GetKey(), (int) It.GetDat());
+      KeySum += It.GetKey();
+      DatSum += It.GetDat();
+      Count++;
+    }
+
+    EXPECT_EQ(0,Count);
+    EXPECT_EQ(0,KeySum);
+    EXPECT_EQ(0,DatSum);
+
+}
+
+// Table manipulations and inserting with >int32 elements
+// Table manipulations
+TEST(TIntInt64H, ManipulateTablewithInt64) {
+  const int64 NElems = 50000;
+  int64 DDist = 10000;
+  const char *FName = "test.hashint.dat";
+  TIntInt64H TableInt;
+  TIntInt64H TableInt1;
+  TIntInt64H TableInt2;
+  int64 i;
+  int64 d;
+  int64 n;
+  int64 Id;
+  int64 Key;
+  int64 DelCount;
+  int64 Count;
+  std::cerr<<"Add table\n";
+  // add table elements
+  d = Prime64(NElems);
+  n = d;
+  std::cerr<<d<<std::endl;
+  for (i = 0; i < NElems; i++) {
+    TableInt.AddDat(n,n+1);
+    n = (n + d) % NElems;
+    if (i%100000000 == 0)
+      std::cerr<<i/100000000<<std::endl;
+  }
+  EXPECT_EQ(0,TableInt.Empty());
+  EXPECT_EQ(NElems,TableInt.Len());
+
+  std::cerr<<"Verify elements by successive keys\n";
+
+  // verify elements by successive keys
+  for (i = 0; i < NElems; i++) {
+    Id = TableInt.GetKeyId(i);
+    EXPECT_EQ(1,Id >= 0);
+    Key = TableInt.GetKey(Id);
+    EXPECT_EQ(0,TableInt.GetDat(Key)-Key-1);
+  }
+
+  std::cerr<<"verify elements by distant keys\n";
+
+  // verify elements by distant keys
+  n = Prime64(d);
+  for (i = 0; i < NElems; i++) {
+    Id = TableInt.GetKeyId(n);
+    EXPECT_EQ(1,Id >= 0);
+    Key = TableInt.GetKey(Id);
+    EXPECT_EQ(0,TableInt.GetDat(Key)-Key-1);
+    n = (n + d) % NElems;
+  }
+
+  std::cerr<<"Verify elements by iterator\n";
+
+  // verify elements by iterator
+  for (TIntInt64H::TIter It = TableInt.BegI(); It < TableInt.EndI(); It++) {
+    EXPECT_EQ(0,It.GetDat()-It.GetKey()-1);
+  }
+
+  std::cerr<<"Verify elements by key index\n";
+  // verify elements by key index
+  Id = TableInt.FFirstKeyId();
+  while (TableInt.FNextKeyId(Id)) {
+    EXPECT_EQ(1,Id >= 0);
+    Key = TableInt.GetKey(Id);
+    EXPECT_EQ(0,TableInt.GetDat(Key)-Key-1);
+  }
+
+  std::cerr<<"Delete elements\n";
+  // delete elements
+  int64 DelKeySum = 0;
+  int64 DelDatSum = 0;
+  DelCount = 0;
+  for (n = 0; n < NElems; n += DDist) {
+    Id = TableInt.GetKeyId(n);
+    //printf("del %d %d %d\n", n, Id, (int) TableInt[Id]);
+    DelKeySum += n;
+    DelDatSum += TableInt[Id];
+    TableInt.DelKeyId(Id);
+    DelCount++;
+
+  }
+  EXPECT_EQ(0,TableInt.Empty());
+  EXPECT_EQ(NElems-DelCount,TableInt.Len());
+  // verify elements by iterator
+  Count = 0;
+  for (TIntInt64H::TIter It = TableInt.BegI(); It < TableInt.EndI(); It++) {
+    EXPECT_EQ(0,It.GetDat()-It.GetKey()-1);
+    //printf("get %d %d\n", (int) It.GetKey(), (int) It.GetDat());
+    Count++;
+  }
+
+  EXPECT_EQ(NElems-DelCount,Count);
+
+  std::cerr<<"Assignment\n";
+  // assignment
+  TableInt1 = TableInt;
+  EXPECT_EQ(0,TableInt1.Empty());
+  EXPECT_EQ(NElems-DelCount,TableInt1.Len());
+
+  // verify elements by iterator
+  Count = 0;
+  for (TIntInt64H::TIter It = TableInt1.BegI(); It < TableInt1.EndI(); It++) {
+    EXPECT_EQ(0,It.GetDat()-It.GetKey()-1);
+    //printf("get %d %d\n", (int) It.GetKey(), (int) It.GetDat());
+    Count++;
+  }
+
+  EXPECT_EQ(NElems-DelCount,Count);
+
+  std::cerr<<"Saving and loading\n";
+  // saving and loading
+  {
+    TFOut FOut(FName);
+    TableInt.Save(FOut);
+    FOut.Flush();
+  }
+
+  {
+    TFIn FIn(FName);
+    TableInt2.Load(FIn);
+  }
+
+  EXPECT_EQ(NElems-DelCount,TableInt2.Len());
+
+  // verify elements by iterator
+  Count = 0;
+  for (TIntInt64H::TIter It = TableInt2.BegI(); It < TableInt2.EndI(); It++) {
+    EXPECT_EQ(0,It.GetDat()-It.GetKey()-1);
+    //printf("get %d %d\n", (int) It.GetKey(), (int) It.GetDat());
+    Count++;
+  }
+
+  EXPECT_EQ(NElems-DelCount,Count);
+  int64 KeySum = 0;
+  int64 DatSum = 0;
+//  std::cerr<<"Remove all elements\n";
+//  // remove all elements
+//  for (i = 0; i < Count; i++) {
+//    Id = TableInt.GetRndKeyId(TInt::Rnd, 0.5);
+//    TableInt.DelKeyId(Id);
+//  }
+//  EXPECT_EQ(0,TableInt.Len());
+//  EXPECT_EQ(1,TableInt.Empty());
+//  std::cerr<<"Verifying elements by iterator after removal\n";
+//  // verify elements by iterator
+//  Count = 0;
+//  for (TIntInt64H::TIter It = TableInt.BegI(); It < TableInt.EndI(); It++) {
+//    EXPECT_EQ(0,It.GetDat()-It.GetKey()-1);
+//    //printf("get %d %d\n", (int) It.GetKey(), (int) It.GetDat());
+//    KeySum += It.GetKey();
+//    DatSum += It.GetDat();
+//    Count++;
+//  }
+//
+//  EXPECT_EQ(0,Count);
+//  EXPECT_EQ(0,KeySum);
+//  EXPECT_EQ(0,DatSum);
+  std::cerr<<"Clear Table\n";
+  // clear the table
+  TableInt1.Clr();
+  EXPECT_EQ(0,TableInt1.Len());
+  EXPECT_EQ(1,TableInt1.Empty());
+
+  // verify elements by iterator
+  KeySum = 0;
+  DatSum = 0;
+  Count = 0;
+  for (TIntInt64H::TIter It = TableInt1.BegI(); It < TableInt1.EndI(); It++) {
+    EXPECT_EQ(0,It.GetDat()-It.GetKey()-1);
+    //printf("get %d %d\n", (int) It.GetKey(), (int) It.GetDat());
+    KeySum += It.GetKey();
+    DatSum += It.GetDat();
+    Count++;
+  }
+
+  EXPECT_EQ(0,Count);
+  EXPECT_EQ(0,KeySum);
+  EXPECT_EQ(0,DatSum);
+}
+
+
+TEST(TIntInt64H, ManipulateTablewithInt64GetRndId) {
+  const int64 NElems = 50000;
+  int64 DDist = 10000;
+  const char *FName = "test.hashint.dat";
+  TIntInt64H TableInt;
+  TIntInt64H TableInt1;
+  TIntInt64H TableInt2;
+  int64 i;
+  int64 d;
+  int64 n;
+  int64 Id;
+  int64 Key;
+  int64 DelCount;
+  int64 Count;
+  std::cerr<<"Add table\n";
+  // add table elements
+  d = Prime64(NElems);
+  n = d;
+  std::cerr<<d<<std::endl;
+  for (i = 0; i < NElems; i++) {
+    TableInt.AddDat(n,n+1);
+    n = (n + d) % NElems;
+    if (i%100000000 == 0)
+      std::cerr<<i/100000000<<std::endl;
+  }
+  EXPECT_EQ(0,TableInt.Empty());
+  EXPECT_EQ(NElems,TableInt.Len());
+
+  std::cerr<<"Remove all elements\n";
+  // remove all elements
+  for (i = 0; i < NElems; i++) {
+    Id = TableInt.GetRndKeyId(TInt::Rnd, 0.8);
+    TableInt.DelKeyId(Id);
+    if (i%10000000 == 0)
+      std::cerr<<i/10000000<<std::endl;
+  }
+  EXPECT_EQ(0,TableInt.Len());
+  EXPECT_EQ(1,TableInt.Empty());
+  std::cerr<<"Verifying elements by iterator after removal\n";
+  // verify elements by iterator
+  int64 KeySum = 0;
+  int64 DatSum = 0;
+  Count = 0;
+  for (TIntInt64H::TIter It = TableInt.BegI(); It < TableInt.EndI(); It++) {
+    EXPECT_EQ(0,It.GetDat()-It.GetKey()-1);
+    //printf("get %d %d\n", (int) It.GetKey(), (int) It.GetDat());
+    KeySum += It.GetKey();
+    DatSum += It.GetDat();
+    Count++;
+  }
+
+  EXPECT_EQ(0,Count);
+  EXPECT_EQ(0,KeySum);
+  EXPECT_EQ(0,DatSum);
+
+}
+
+
+bool IsPrime64(const int64& d) {
+  int64 i;
+  int64 j;
+
+  // 1, 2, 3 are primes
+  if (d < 4  ||  (d & 0x01) == 0) {
+    return true;
+  }
+
+  // even numbers are not primes
+  if (d < 4  ||  (d & 0x01) == 0) {
+    return true;
+  }
+
+  // test divisions with < sqrt(d)
+  for (i = 2; i*i <= d; i++) {
+    j = d/i;
+    if (j*i == d) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+int64 Prime64(const int64& n) {
+  int64 d;
+
+  d = (int64) ((float) n * 0.61803398875);    // golden ratio
+  d |= 0x01;                                // get an odd number
+  while (!IsPrime64(d)) {
+    d += 2;
+  }
+
+  return d;
+}
+
+
--- test/test-TUNGraph64.cpp
+++ test/test-TUNGraph64.cpp
@@ -1,55 +1,41 @@
 #include <gtest/gtest.h>
 
 #include "Snap.h"
-
-// Test the default constructor
-TEST(TUNGraph, DefaultConstructor) {
-  PUNGraph Graph;
-
-  Graph = TUNGraph::New();
-
-  EXPECT_EQ(0,Graph->GetNodes());
-  EXPECT_EQ(0,Graph->GetEdges());
-
-  EXPECT_EQ(1,Graph->IsOk());
-  EXPECT_EQ(1,Graph->Empty());
-  EXPECT_EQ(0,Graph->HasFlag(gfDirected));
-}
-
 // Test node, edge creation
-TEST(TUNGraph, ManipulateNodesEdges) {
-  int NNodes = 1000;
-  int NEdges = 10000;
+TEST(TUNGraph, ManipulateNodesEdgesWith64BitNodeValues) {
+  int64 NNodes = 1000;
+  int64 NEdges = 10000;
+  int64 OFFSET = 1000000000000;
   const char *FName = "test.graph.dat";
 
   PUNGraph Graph;
   PUNGraph Graph1;
   PUNGraph Graph2;
-  int i;
-  int n;
-  int NCount;
-  int LCount;
-  int x,y;
-  int Deg, InDeg, OutDeg;
+  int64 i;
+  int64 n;
+  int64 NCount;
+  int64 LCount;
+  int64 x,y;
+  int64 Deg, InDeg, OutDeg;
 
   Graph = TUNGraph::New();
   EXPECT_EQ(1,Graph->Empty());
 
   // create the nodes
   for (i = 0; i < NNodes; i++) {
-    Graph->AddNode(i);
+    Graph->AddNode(OFFSET + i);
   }
   EXPECT_EQ(0,Graph->Empty());
   EXPECT_EQ(NNodes,Graph->GetNodes());
-  
+
   // create random edges
   NCount = NEdges;
   LCount = 0;
   while (NCount > 0) {
     x = (long) (drand48() * NNodes);
     y = (long) (drand48() * NNodes);
-    if (!Graph->IsEdge(x,y)) {
-      n = Graph->AddEdge(x, y);
+    if (!Graph->IsEdge(OFFSET + x,OFFSET + y)) {
+      n = Graph->AddEdge(OFFSET +x,OFFSET + y);
       NCount--;
       if (x == y) {
         LCount++;
@@ -63,12 +49,12 @@ TEST(TUNGraph, ManipulateNodesEdges) {
   EXPECT_EQ(1,Graph->IsOk());
 
   for (i = 0; i < NNodes; i++) {
-    EXPECT_EQ(1,Graph->IsNode(i));
+    EXPECT_EQ(1,Graph->IsNode(OFFSET + i));
   }
 
-  EXPECT_EQ(0,Graph->IsNode(NNodes));
-  EXPECT_EQ(0,Graph->IsNode(NNodes+1));
-  EXPECT_EQ(0,Graph->IsNode(2*NNodes));
+  EXPECT_EQ(0,Graph->IsNode(OFFSET + NNodes));
+  EXPECT_EQ(0,Graph->IsNode(OFFSET + NNodes+1));
+  EXPECT_EQ(0,Graph->IsNode(OFFSET + 2*NNodes));
 
   // nodes iterator
   NCount = 0;
@@ -151,44 +137,45 @@ TEST(TUNGraph, ManipulateNodesEdges) {
 }
 
 // Test edge iterator, manipulate edges
-TEST(TUNGraph, ManipulateEdges) {
-  int Iterations = 100;
-  
-  int NNodes;
-  int NNodesStart = 8;
-  int NNodesEnd = 25;
-  
-  int NEdges;
-  int NEdgesStart = 0;
-  int NEdgesEnd = 50;
-  
+TEST(TUNGraph, ManipulateEdgesWith64BitNodeValues) {
+  int64 Iterations = 100;
+
+  int64 NNodes;
+  int64 NNodesStart = 8;
+  int64 NNodesEnd = 25;
+
+  int64 NEdges;
+  int64 NEdgesStart = 0;
+  int64 NEdgesEnd = 50;
+
   PUNGraph Graph;
   PUNGraph Graph1;
   PUNGraph Graph2;
-  int NCount, ECount;
-  int x,y;
-  TIntV NodeIds;
-  THashSet<TIntPr> EdgeSet;
+  int64 NCount, ECount;
+  int64 x,y;
+  TInt64V NodeIds;
+  int64 OFFSET = 1000000000000;
+  THashSet<TInt64Pr, int64> EdgeSet;
   TInt::Rnd.PutSeed(0);
-  
+
   for (int i = 0; i < Iterations; i++) {
-    
+
     for (NEdges = NEdgesStart; NEdges <= NEdgesEnd; NEdges++) {
-      
+
       for (NNodes = NNodesStart; NNodes <= NNodesEnd; NNodes++) {
-        
+
         // Skip if too many edges required per NNodes (n*(n+1)/2)
         if (NEdges > (NNodes * (NNodes+1)/2)) {
           continue;
         }
-        
+
         Graph = TUNGraph::New();
         EXPECT_TRUE(Graph->Empty());
-        
+
         // Generate NNodes
         NodeIds.Gen(NNodes);
         for (int n = 0; n < NNodes; n++) {
-          NodeIds[n] = n;
+          NodeIds[n] = OFFSET + n;
         }
         // Add the nodes in random order
         NodeIds.Shuffle(TInt::Rnd);
@@ -196,29 +183,29 @@ TEST(TUNGraph, ManipulateEdges) {
           Graph->AddNode(NodeIds[n]);
         }
         EXPECT_FALSE(Graph->Empty());
-        
+
         // Create random edges
         EdgeSet.Clr();
         NCount = NEdges;
         while (NCount > 0) {
           x = (long) (drand48() * NNodes);
           y = (long) (drand48() * NNodes);
-          
-          if (!Graph->IsEdge(x,y)) {
-            Graph->AddEdge(x, y);
-            EdgeSet.AddKey(TIntPr(x, y));
-            EdgeSet.AddKey(TIntPr(y, x));
+
+          if (!Graph->IsEdge(OFFSET + x,OFFSET + y)) {
+            Graph->AddEdge(OFFSET + x, OFFSET + y);
+            EdgeSet.AddKey(TInt64Pr(OFFSET + x, OFFSET + y));
+            EdgeSet.AddKey(TInt64Pr(OFFSET + y, OFFSET + x));
             NCount--;
           }
         }
-        
+
         // Check edge iterator to make sure all edges are valid and no more (in hash set)
-        TIntPrV DelEdgeV;
+        TIntPr64V DelEdgeV;
         for (TUNGraph::TEdgeI EI = Graph->BegEI(); EI < Graph->EndEI(); EI++) {
-          
-          TIntPr Edge(EI.GetSrcNId(), EI.GetDstNId());
-          TIntPr EdgeR(EI.GetDstNId(), EI.GetSrcNId());
-          
+
+          TInt64Pr Edge(EI.GetSrcNId(), EI.GetDstNId());
+          TInt64Pr EdgeR(EI.GetDstNId(), EI.GetSrcNId());
+
           EXPECT_TRUE(EdgeSet.IsKey(Edge) || EdgeSet.IsKey(EdgeR));
           if (EdgeSet.IsKey(Edge)) {
               EdgeSet.DelKey(Edge);
@@ -227,37 +214,37 @@ TEST(TUNGraph, ManipulateEdges) {
             EdgeSet.DelKey(EdgeR);
           }
           DelEdgeV.Add(Edge);
-          
+
         }
         EXPECT_TRUE(EdgeSet.Empty());
         EXPECT_TRUE(DelEdgeV.Len() == NEdges);
-        
+
         // Randomly delete node, check to make sure edges were deleted
         NodeIds.Shuffle(TInt::Rnd);
         for (int n = 0; n < NNodes; n++) {
 
-          TIntPrV DelEdgeNodeV;
-          int DelNodeId = NodeIds[n];
-          
+          TIntPr64V DelEdgeNodeV;
+          int64 DelNodeId = NodeIds[n];
+
           // Get edges for node to be deleted (to verify all deleted)
-          int EdgesBeforeDel;
+          int64 EdgesBeforeDel;
           EdgesBeforeDel = Graph->GetEdges();
           for (TUNGraph::TEdgeI EI = Graph->BegEI(); EI < Graph->EndEI(); EI++) {
             if (EI.GetSrcNId() == DelNodeId || EI.GetDstNId() == DelNodeId) {
-              DelEdgeNodeV.Add(TIntPr(EI.GetSrcNId(), EI.GetDstNId()));
+              DelEdgeNodeV.Add(TInt64Pr(EI.GetSrcNId(), EI.GetDstNId()));
             }
           }
-          
+
           Graph->DelNode(DelNodeId);
           EXPECT_TRUE(EdgesBeforeDel == DelEdgeNodeV.Len() + Graph->GetEdges());
           EXPECT_FALSE(Graph->IsNode(DelNodeId));
           EXPECT_TRUE(Graph->IsOk());
-          
+
           // Make sure all the edges to deleted node are gone
           for (int e = 0; e < DelEdgeNodeV.Len(); e++) {
             EXPECT_FALSE(Graph->IsEdge(DelEdgeNodeV[e].Val1, DelEdgeNodeV[e].Val2));
           }
-          
+
           // Make sure no edge on graph is connected to deleted node
           ECount = 0;
           for (TUNGraph::TEdgeI EI = Graph->BegEI(); EI < Graph->EndEI(); EI++) {
@@ -277,19 +264,152 @@ TEST(TUNGraph, ManipulateEdges) {
 
 }
 
-// Test small graph
-TEST(TUNGraph, GetSmallGraph) {
+// Test node, edge creation
+TEST(TUNGraph, ManipulateNodesEdges64Bit) {
+  int64 NNodes = 1000;
+  int64 NOutDeg = 50; // should be < NNodes
+  int64 NEdges = NOutDeg*NNodes + NNodes;
+  const int64 OFFSET = 0;
+  const char *FName = "test.graph.dat";
+
   PUNGraph Graph;
+  PUNGraph Graph1;
+  PUNGraph Graph2;
+  int64 i,j;
+  int64 n;
+  int64 LCount = 0, NCount = 0;
+  int64 Deg, InDeg, OutDeg;
+  Graph = TUNGraph::New();
+  EXPECT_EQ(1,Graph->Empty());
+  std::cerr<<"Creating nodes\n";
+  // create the nodes
+  for (i = 0; i < NNodes; i++) {
+    Graph->AddNode(OFFSET + i);
+  }
+  EXPECT_EQ(0,Graph->Empty());
+  EXPECT_EQ(NNodes,Graph->GetNodes());
+  std::cerr<<"Creating random edges\n";
 
-  Graph = TUNGraph::GetSmallGraph();
+  // create random edges
+  for (i=0; i < NNodes; i++){
+    for (j = 1; j <= NOutDeg; j++){
+      Graph->AddEdge(OFFSET +i,OFFSET + (i + j)%NNodes);
+      NCount++;
+    }
+    Graph->AddEdge(OFFSET + i,OFFSET + i);
+    NCount++;
+    LCount++;
+    if (i%10000000 == 0)
+      std::cerr<<i/10000000<<std::endl;
+  }
 
-  EXPECT_EQ(5,Graph->GetNodes());
-  EXPECT_EQ(5,Graph->GetEdges());
+  EXPECT_EQ(NEdges,NCount);
+  EXPECT_EQ(NEdges,Graph->GetEdges());
+
+  EXPECT_EQ(0,Graph->Empty());
+  EXPECT_EQ(1,Graph->IsOk());
+
+  for (i = 0; i < NNodes; i++) {
+    EXPECT_EQ(1,Graph->IsNode(OFFSET + i));
+  }
+
+  EXPECT_EQ(0,Graph->IsNode(OFFSET + NNodes));
+  EXPECT_EQ(0,Graph->IsNode(OFFSET + NNodes+1));
+  EXPECT_EQ(0,Graph->IsNode(OFFSET + 2*NNodes));
+  std::cerr<<"nodes iterator\n";
+
+  // nodes iterator
+  NCount = 0;
+  for (TUNGraph::TNodeI NI = Graph->BegNI(); NI < Graph->EndNI(); NI++) {
+    NCount++;
+  }
+  EXPECT_EQ(NNodes,NCount);
+  std::cerr<<"edges per node iterator\n";
+
+  // edges per node iterator, each non-loop is visited twice, each loop once
+  NCount = 0;
+  bool flag = false;
+  for (TUNGraph::TNodeI NI = Graph->BegNI(); NI < Graph->EndNI(); NI++) {
+    int64 nid = NI.GetId();
+    for (int64 e = 0; e < NI.GetOutDeg(); e++) {
+      int64 nbrid = NI.GetOutNId(e);
+      if (nid > OFFSET + 2*NOutDeg && nid < OFFSET + NNodes - 2*NOutDeg)
+        if (nbrid < nid - NOutDeg || nbrid > nid + NOutDeg)
+          flag = true;
+      NCount++;
+    }
+  }
+  EXPECT_EQ(NEdges*2-LCount,NCount);
+  EXPECT_EQ(flag,false);
+  std::cerr<<"Edge iterator\n";
+
+  // edges iterator, each edge is visited once
+  NCount = 0;
+  for (TUNGraph::TEdgeI EI = Graph->BegEI(); EI < Graph->EndEI(); EI++) {
+    NCount++;
+  }
+  EXPECT_EQ(NEdges,NCount);
+  std::cerr<<"Node degree\n";
+
+  // node degree
+  for (TUNGraph::TNodeI NI = Graph->BegNI(); NI < Graph->EndNI(); NI++) {
+    Deg = NI.GetDeg();
+    InDeg = NI.GetInDeg();
+    OutDeg = NI.GetOutDeg();
+
+    EXPECT_EQ(Deg,InDeg);
+    EXPECT_EQ(Deg,OutDeg);
+  }
+  std::cerr<<"Assignment\n";
+
+  // assignment
+  Graph1 = TUNGraph::New();
+  *Graph1 = *Graph;
+
+  EXPECT_EQ(NNodes,Graph1->GetNodes());
+  EXPECT_EQ(NEdges,Graph1->GetEdges());
+  EXPECT_EQ(0,Graph1->Empty());
+  EXPECT_EQ(1,Graph1->IsOk());
+  std::cerr<<"Saving and loading\n";
+
+  // saving and loading
+  {
+    TFOut FOut(FName);
+    Graph->Save(FOut);
+    FOut.Flush();
+  }
+
+  {
+    TFIn FIn(FName);
+    Graph2 = TUNGraph::Load(FIn);
+  }
+
+  EXPECT_EQ(NNodes,Graph2->GetNodes());
+  EXPECT_EQ(NEdges,Graph2->GetEdges());
+  EXPECT_EQ(0,Graph2->Empty());
+  EXPECT_EQ(1,Graph2->IsOk());
+  std::cerr<<"remove all nodes and edges\n";
+
+  // remove all the nodes and edges
+  for (i = 0; i < NNodes; i++) {
+    n = Graph->GetRndNId();
+    Graph->DelNode(n);
+    if(i%1000000 == 0)
+          std::cerr<<i/1000000<<std::endl;
+  }
+
+  EXPECT_EQ(0,Graph->GetNodes());
+  EXPECT_EQ(0,Graph->GetEdges());
 
   EXPECT_EQ(1,Graph->IsOk());
-  EXPECT_EQ(0,Graph->Empty());
-  EXPECT_EQ(0,Graph->HasFlag(gfDirected));
+  EXPECT_EQ(1,Graph->Empty());
+  std::cerr<<"Clear graph\n";
+
+  Graph1->Clr();
+
+  EXPECT_EQ(0,Graph1->GetNodes());
+  EXPECT_EQ(0,Graph1->GetEdges());
+
+  EXPECT_EQ(1,Graph1->IsOk());
+  EXPECT_EQ(1,Graph1->Empty());
 }
-
-
-
