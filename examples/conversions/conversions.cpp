33ccc19407d715b2ebcd4f5a2c9da240573a25e1
gaspar09 <gaspar09@madmax3.stanford.edu>
2018-02-22 21:30:32 -0800

    Minimal option parsing on conversions.

--- examples/conversions/conversions.cpp
+++ examples/conversions/conversions.cpp
@@ -1,75 +1,74 @@
 #include "stdafx.h"
 #include <time.h>
 
-void printTime(PTmProfiler tm, int id) {
-  printf("%s: %.2f\n", tm->GetTimerNm(id).CStr(), tm->GetTimerSec(id));
+void printConversionTime(clock_t t0, clock_t t1, TUInt64 nnodes, TUInt64 nedges, const char * ConversionType) {
+    printf("Convert TUNGraph->%s (%s nodes, %s edges): %f\n",
+	   ConversionType,
+  	   TUInt64::GetStr(nnodes).CStr(),
+  	   TUInt64::GetStr(nedges).CStr(),	   
+  	   ((float)t1-t0)/CLOCKS_PER_SEC);
 }
 
+
 int main(int argc, char* argv[]) {
-  //// what type of graph do you want to use?
-  typedef PUNGraph PGraph; // undirected graph
-  //typedef PNGraph PGraph;  //   directed graph
-  //typedef PNEGraph PGraph;  //   directed multigraph
-  //typedef TPt<TNodeNet<TInt> > PGraph;
-  //typedef TPt<TNodeEdgeNet<TInt, TInt> > PGraph;
+
+  Env = TEnv(argc, argv, TNotify::StdNotify);
+  Env.PrepArgs(TStr::Fmt("ragm. build: %s, %s. Time: %s", __TIME__, __DATE__, TExeTm::GetCurTm()));
+  
+  const bool useTwitter = Env.GetIfArgPrefixBool("-t:", false, "Run conversions on Twitter Network");
+
+  const bool runAll = Env.GetIfArgPrefixBool("-a:", false, "Run all conversions");
+  const bool runUNDir = Env.GetIfArgPrefixBool("-u:", false, "Run conversion: TUNGraph->TUNGraph");  
+  const bool runDir = Env.GetIfArgPrefixBool("-d:", false, "Run conversion: TUNGraph->TNGraph");
+  const bool runNet = Env.GetIfArgPrefixBool("-e:", false, "Run conversion: TUNGraph->TNEANet");
+
 
   // File input
-  const char * binaryUNGraph = "/lfs/local/0/gaspar09/networks/twitter_rv.graph";
+  const char * binaryUNGraph = NULL;
+  if (useTwitter) {
+    binaryUNGraph = "/lfs/local/0/gaspar09/networks/twitter_rv.graph";
+    printf("Twitter Graph\n");
+  } else {
+    binaryUNGraph = "/lfs/local/0/gaspar09/networks/soc-LiveJournal1.graph";
+    printf("Journal Graph\n");
+  }
   //const char * binaryNGraph = "/lfs/local/0/gaspar09/networks/twitter_rv.ngraph";
-  //const char * binaryNEANet = "/lfs/local/0/gaspar09/networks/twitter_rv.neanet";
-
-  binaryUNGraph = "/lfs/local/0/gaspar09/networks/soc-LiveJournal1.graph";
-  //binaryNGraph = "/lfs/local/0/gaspar09/networks/soc-LiveJournal1.ngraph";
-  //binaryNEANet = "/lfs/local/0/gaspar09/networks/soc-LiveJournal1.neanet";
-
-
-  printf("Benchmark Graph Load Binary\n");
-
+  //const char * binaryNEANet = "/lfs/local/0/gaspar09/networks/twitter_rv.neanet";  
+  
+  //const char * binaryNGraph = "/lfs/local/0/gaspar09/networks/soc-LiveJournal1.ngraph";
+  //const char * binaryNEANet = "/lfs/local/0/gaspar09/networks/soc-LiveJournal1.neanet";
   PSIn SIn_p = TFIn::New(binaryUNGraph);
 
   clock_t t0 = clock();
-  PGraph G = TUNGraph::Load(*SIn_p);
+  PUNGraph G = TUNGraph::Load(*SIn_p);
   clock_t t1 = clock();
   printf("Load TUNGraph (%s nodes, %s edges) Binary: %f\n",
 	 TUInt64::GetStr(G->GetNodes()).CStr(),
 	 TUInt64::GetStr(G->GetEdges()).CStr(),
 	 ((float)t1-t0)/CLOCKS_PER_SEC);
-
-  printf("Benchmark Graph Conversions\n");
  
-  // get UNGraph
-  {
+  if (runAll || runUNDir) {
+    // get UNGraph
     clock_t t0 = clock();
-    PUNGraph UNG = TSnap::ConvertGraph<PUNGraph>(G);
+    PUNGraph H = TSnap::ConvertGraph<PUNGraph>(G);
     clock_t t1 = clock();
-    printf("Convert TUNGraph->TUNGraph (%s nodes, %s edges): %f\n",
-  	   TUInt64::GetStr(UNG->GetNodes()).CStr(),
-  	   TUInt64::GetStr(UNG->GetEdges()).CStr(),	   
-  	   ((float)t1-t0)/CLOCKS_PER_SEC);
-  }
-  
-  // get NGraph
-  {
-    clock_t t0 = clock();
-    PNGraph NG = TSnap::ConvertGraph<PNGraph>(G);
-    clock_t t1 = clock();
-    printf("Convert TUNGraph->TNGraph (%s nodes, %s edges) : %f\n",
-  	   TUInt64::GetStr(NG->GetNodes()).CStr(),
-  	   TUInt64::GetStr(NG->GetEdges()).CStr(),
-  	   ((float)t1-t0)/CLOCKS_PER_SEC);
-  }
+    printConversionTime(t0, t1, H->GetNodes(), H->GetEdges(), "TUNGraph");
 
-  // get NEGraph
-  {
+  }
+  if (runAll || runDir) {
+    // get NGraph
     clock_t t0 = clock();
-    PNEANet NEG = TSnap::ConvertGraph<PNEANet>(G);
+    PNGraph H = TSnap::ConvertGraph<PNGraph>(G);
     clock_t t1 = clock();
-    printf("Convert TUNGraph->TNEANet (%s nodes, %s edges): %f\n",
-	   TUInt64::GetStr(NEG->GetNodes()).CStr(),
-	   TUInt64::GetStr(NEG->GetEdges()).CStr(),
-	   ((float)t1-t0)/CLOCKS_PER_SEC);
+    printConversionTime(t0, t1, H->GetNodes(), H->GetEdges(), "TNGraph");
+  }
+  if (runAll || runNet) {
+    // get NEGraph
+    clock_t t0 = clock();
+    PNEANet H = TSnap::ConvertGraph<PNEANet>(G);
+    clock_t t1 = clock();
+    printConversionTime(t0, t1, H->GetNodes(), H->GetEdges(), "TNEANet");
   }
 
   return 0;
 }
-
