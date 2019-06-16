cde2c4270419d4283ded92f713bcf2e6642b3de4
Lam Vu <lamvuvan.hust@gmail.com>
2019-06-16 09:00:48 -0700

    fix community and centrality modules

--- examples/centrality/centrality.cpp
+++ examples/centrality/centrality.cpp
@@ -1,62 +1,71 @@
 #include "stdafx.h"
 
-int main(int argc, char* argv[]) {
-  Env = TEnv(argc, argv, TNotify::StdNotify);
-  Env.PrepArgs(TStr::Fmt("Node Centrality. build: %s, %s. Time: %s", __TIME__, __DATE__, TExeTm::GetCurTm()));
-  TExeTm ExeTm;
-  Try
-  const TStr InFNm = Env.GetIfArgPrefixStr("-i:", argv[2], "Input un/directed graph");
-  const TStr OutFNm = Env.GetIfArgPrefixStr("-o:", argv[4], "Output file");
-  printf("Loading %s...", InFNm.CStr());
-  PNGraph Graph = TSnap::LoadEdgeList<PNGraph>(InFNm);
-  //PNGraph Graph = TSnap::GenRndGnm<PNGraph>(10, 10);
-  //TGraphViz::Plot(Graph, gvlNeato, InFNm+".gif", InFNm, true);
-  printf("nodes:%s  edges:%s\n",
-        TUInt64::GetStr(Graph->GetNodes()).CStr(),
-        TUInt64::GetStr(Graph->GetEdges()).CStr());
-  PUNGraph UGraph = TSnap::ConvertGraph<PUNGraph>(Graph); // undirected version of the graph
-  TIntFlt64H BtwH, EigH;
-  TIntFlt64H PRankH;
-  TIntFlt64H HubH, AuthH;
-  TIntFlt64H CcfH;
-  TIntFlt64H CloseH;
-  //printf("Computing...\n");
-  printf("Treat graph as DIRECTED: ");
-  printf(" PageRank... ");             TSnap::GetPageRank(Graph, PRankH, 0.85);
-  printf(" Hubs&Authorities...");      TSnap::GetHits(Graph, HubH, AuthH);
-  printf("\nTreat graph as UNDIRECTED: ");
-  printf(" Eigenvector...");           TSnap::GetEigenVectorCentr(UGraph, EigH);
-  printf(" Clustering...");            TSnap::GetNodeClustCf(UGraph, CcfH);
-  printf(" Betweenness (SLOW!)...");   TSnap::GetBetweennessCentr(UGraph, BtwH, 1);
-  printf(" Constraint (SLOW!)...");    TNetConstraint<PUNGraph> NetC(UGraph, true);
-  printf(" Closeness (SLOW!)...");
-  for (TUNGraph::TNodeI NI = UGraph->BegNI(); NI < UGraph->EndNI(); NI++) {
-    const int64 NId = NI.GetId();
-    CloseH.AddDat(NId, TSnap::GetClosenessCentr<PUNGraph>(UGraph, NId, false));
-  }
-  printf("\nDONE! saving...");
-  FILE *F = fopen(OutFNm.CStr(), "wt");
-  fprintf(F,"#Network: %s\n", InFNm.CStr());
-  fprintf(F,"#Nodes: %s\tEdges: %s\n",
-        TUInt64::GetStr(Graph->GetNodes()).CStr(),
-        TUInt64::GetStr(Graph->GetEdges()).CStr());
-  fprintf(F,"#NodeId\tDegree\tCloseness\tBetweennes\tEigenVector\tNetworkConstraint\tClusteringCoefficient\tPageRank\tHubScore\tAuthorityScore\n");
-  for (TUNGraph::TNodeI NI = UGraph->BegNI(); NI < UGraph->EndNI(); NI++) {
-    const int64 NId = NI.GetId();
-    const double DegCentr = UGraph->GetNI(NId).GetDeg();
-    const double CloCentr = CloseH.GetDat(NId);
-    const double BtwCentr = BtwH.GetDat(NId);
-    const double EigCentr = EigH.GetDat(NId);
-    const double Constraint = NetC.GetNodeC(NId);
-    const double ClustCf = CcfH.GetDat(NId);
-    const double PgrCentr = PRankH.GetDat(NId);
-    const double HubCentr = HubH.GetDat(NId);
-    const double AuthCentr = AuthH.GetDat(NId);
-    fprintf(F, "%d\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n", NId, 
-      DegCentr, CloCentr, BtwCentr, EigCentr, Constraint, ClustCf, PgrCentr, HubCentr, AuthCentr);
-  }
-  fclose(F);
-  Catch
-  printf("\nrun time: %s (%s)\n", ExeTm.GetTmStr(), TSecTm::GetCurTm().GetTmStr().CStr());
-  return 0;
+int main(int argc, char *argv[]) {
+    Env = TEnv(argc, argv, TNotify::StdNotify);
+    Env.PrepArgs(TStr::Fmt("Node Centrality. build: %s, %s. Time: %s", __TIME__, __DATE__, TExeTm::GetCurTm()));
+    TExeTm ExeTm;
+    Try
+    const TStr InFNm = Env.GetIfArgPrefixStr("-i:", argv[2], "Input un/directed graph");
+    const TStr OutFNm = Env.GetIfArgPrefixStr("-o:", argv[4], "Output file");
+    printf("Loading %s...", InFNm.CStr());
+    PNGraph Graph = TSnap::LoadEdgeList<PNGraph>(InFNm);
+    //PNGraph Graph = TSnap::GenRndGnm<PNGraph>(10, 10);
+    //TGraphViz::Plot(Graph, gvlNeato, InFNm+".gif", InFNm, true);
+    printf("nodes:%s  edges:%s\n",
+           TUInt64::GetStr(Graph->GetNodes()).CStr(),
+           TUInt64::GetStr(Graph->GetEdges()).CStr());
+    PUNGraph UGraph = TSnap::ConvertGraph<PUNGraph>(Graph); // undirected version of the graph
+    TIntFlt64H BtwH, EigH;
+    TIntFlt64H PRankH;
+    TIntFlt64H HubH, AuthH;
+    TIntFlt64H CcfH;
+    TIntFlt64H CloseH;
+    printf("Computing...\n");
+    printf("Treat graph as DIRECTED: ");
+    printf(" PageRank... ");
+    TSnap::GetPageRank_v1(Graph, PRankH, 0.85);
+    printf(" Hubs&Authorities...");
+    TSnap::GetHits(Graph, HubH, AuthH);
+    printf("\nTreat graph as UNDIRECTED: ");
+    printf(" Eigenvector...");
+    TSnap::GetEigenVectorCentr(UGraph, EigH);
+    printf(" Clustering...");
+    TSnap::GetNodeClustCf(UGraph, CcfH);
+    printf(" Betweenness (SLOW!)...");
+    TSnap::GetBetweennessCentr(UGraph, BtwH, 1);
+    printf(" Constraint (SLOW!)...");
+    TNetConstraint <PUNGraph> NetC(UGraph, true);
+    printf(" Closeness (SLOW!)...");
+    for (TUNGraph::TNodeI NI = UGraph->BegNI(); NI < UGraph->EndNI(); NI++) {
+        const int64 NId = NI.GetId();
+        CloseH.AddDat(NId, TSnap::GetClosenessCentr<PUNGraph>(UGraph, NId, false));
+    }
+    printf("\nDONE! saving...");
+    FILE *F = fopen(OutFNm.CStr(), "wt");
+    fprintf(F, "#Network: %s\n", InFNm.CStr());
+    fprintf(F, "#Nodes: %s\tEdges: %s\n",
+            TUInt64::GetStr(Graph->GetNodes()).CStr(),
+            TUInt64::GetStr(Graph->GetEdges()).CStr());
+    fprintf(F,
+            "#NodeId\tDegree\tCloseness\tBetweennes\tEigenVector\tNetworkConstraint\tClusteringCoefficient\tPageRank\tHubScore\tAuthorityScore\n");
+    for (TUNGraph::TNodeI NI = UGraph->BegNI(); NI < UGraph->EndNI(); NI++) {
+        const int64 NId = NI.GetId();
+        const double DegCentr = UGraph->GetNI(NId).GetDeg();
+        const double CloCentr = CloseH.GetDat(NId);
+        const double BtwCentr = BtwH.GetDat(NId);
+        const double EigCentr = EigH.GetDat(NId);
+        const double Constraint = NetC.GetNodeC(NId);
+        const double ClustCf = CcfH.GetDat(NId);
+        const double PgrCentr = PRankH.GetDat(NId);
+        const double HubCentr = HubH.GetDat(NId);
+        const double AuthCentr = AuthH.GetDat(NId);
+//        fprintf(F, "%lli\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n", NId,
+//                DegCentr, CloCentr, BtwCentr, EigCentr, Constraint, PgrCentr, HubCentr, AuthCentr);
+        fprintf(F, "%lli\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n", NId,
+                DegCentr, CloCentr, BtwCentr, EigCentr, Constraint, ClustCf, PgrCentr, HubCentr, AuthCentr);
+    }
+    fclose(F);
+    Catch
+            printf("\nrun time: %s (%s)\n", ExeTm.GetTmStr(), TSecTm::GetCurTm().GetTmStr().CStr());
+    return 0;
 }
--- examples/community/community.cpp
+++ examples/community/community.cpp
@@ -1,58 +1,60 @@
 #include "stdafx.h"
 
-int main(int argc, char* argv[]) {
-  Env = TEnv(argc, argv, TNotify::StdNotify);
-  Env.PrepArgs(TStr::Fmt("Network community detection. build: %s, %s. Time: %s", __TIME__, __DATE__, TExeTm::GetCurTm()));
-  TExeTm ExeTm;
-  Try
-  const TStr InFNm = Env.GetIfArgPrefixStr("-i:", argv[2], "Input graph (undirected graph)");
-  const TStr OutFNm = Env.GetIfArgPrefixStr("-o:", argv[4], "Output file");
-  const int CmtyAlg = Env.GetIfArgPrefixInt("-a:", 2, "Algorithm: 1:Girvan-Newman, 2:Clauset-Newman-Moore, 3:Infomap");
+int main(int argc, char *argv[]) {
+    Env = TEnv(argc, argv, TNotify::StdNotify);
+    Env.PrepArgs(
+            TStr::Fmt("Network community detection. build: %s, %s. Time: %s", __TIME__, __DATE__, TExeTm::GetCurTm()));
+    TExeTm ExeTm;
+    Try
+    const TStr InFNm = Env.GetIfArgPrefixStr("-i:", argv[2], "Input graph (undirected graph)");
+    const TStr OutFNm = Env.GetIfArgPrefixStr("-o:", argv[4], "Output file");
+    const int CmtyAlg = Env.GetIfArgPrefixInt("-a:", 2,
+                                              "Algorithm: 1:Girvan-Newman, 2:Clauset-Newman-Moore, 3:Infomap");
 
-  PUNGraph Graph = TSnap::LoadEdgeList<PUNGraph>(InFNm, false);
-  //PUNGraph Graph = TSnap::LoadEdgeList<PUNGraph>("../as20graph.txt", false);
-  //PUNGraph Graph = TSnap::GenRndGnm<PUNGraph>(5000, 10000); // generate a random graph
+    PUNGraph Graph = TSnap::LoadEdgeList<PUNGraph>(InFNm, false);
+    //PUNGraph Graph = TSnap::LoadEdgeList<PUNGraph>("../as20graph.txt", false);
+    //PUNGraph Graph = TSnap::GenRndGnm<PUNGraph>(5000, 10000); // generate a random graph
 
-  TSnap::DelSelfEdges(Graph);
-  TCnComV CmtyV;
-  double Q = 0.0;
-  TStr CmtyAlgStr;
-  if (CmtyAlg == 1) {
-    CmtyAlgStr = "Girvan-Newman";
-    Q = TSnap::CommunityGirvanNewman(Graph, CmtyV); }
-  else if (CmtyAlg == 2) {
-    CmtyAlgStr = "Clauset-Newman-Moore";
-    Q = TSnap::CommunityCNM(Graph, CmtyV); }
-  else if (CmtyAlg == 3) {
-    CmtyAlgStr = "Infomap";
-    Q = TSnap::Infomap(Graph, CmtyV); }
-  else { Fail; }
+    TSnap::DelSelfEdges(Graph);
+    TCnComV CmtyV;
+    double Q = 0.0;
+    TStr CmtyAlgStr;
+    if (CmtyAlg == 1) {
+        CmtyAlgStr = "Girvan-Newman";
+        Q = TSnap::CommunityGirvanNewman(Graph, CmtyV);
+    } else if (CmtyAlg == 2) {
+        CmtyAlgStr = "Clauset-Newman-Moore";
+        Q = TSnap::CommunityCNM(Graph, CmtyV);
+    } else if (CmtyAlg == 3) {
+        CmtyAlgStr = "Infomap";
+        Q = TSnap::Infomap(Graph, CmtyV);
+    } else { Fail; }
 
-  FILE *F = fopen(OutFNm.CStr(), "wt");
-  fprintf(F, "# Input: %s\n", InFNm.CStr());
-  fprintf(F, "# Nodes: %s    Edges: %s\n",
-        TUInt64::GetStr(Graph->GetNodes()).CStr(),
-        TUInt64::GetStr(Graph->GetEdges()).CStr());
+    FILE *F = fopen(OutFNm.CStr(), "wt");
+    fprintf(F, "# Input: %s\n", InFNm.CStr());
+    fprintf(F, "# Nodes: %s    Edges: %s\n",
+            TUInt64::GetStr(Graph->GetNodes()).CStr(),
+            TUInt64::GetStr(Graph->GetEdges()).CStr());
 
-  fprintf(F, "# Algoritm: %s\n", CmtyAlgStr.CStr());
-  if (CmtyAlg!=3) {
-    fprintf(F, "# Modularity: %f\n", Q);
-  } else {
-    fprintf(F, "# Average code length: %f\n", Q);
-  }
-  fprintf(F, "# Communities: %s\n", TInt64::GetStr(CmtyV.Len()).CStr());
-
-  fprintf(F, "# NId\tCommunityId\n");
-  for (int64 c = 0; c < CmtyV.Len(); c++) {
-    for (int64 i = 0; i < CmtyV[c].Len(); i++) {
-      fprintf(F, "%s\t%s\n",
-                TInt64::GetStr(CmtyV[c][i].Val).CStr(),
-                TInt64::GetStr(c).CStr());
+    fprintf(F, "# Algoritm: %s\n", CmtyAlgStr.CStr());
+    if (CmtyAlg != 3) {
+        fprintf(F, "# Modularity: %f\n", Q);
+    } else {
+        fprintf(F, "# Average code length: %f\n", Q);
     }
-  }
-  fclose(F);
+    fprintf(F, "# Communities: %s\n", TInt64::GetStr(CmtyV.Len()).CStr());
 
-  Catch
-  printf("\nrun time: %s (%s)\n", ExeTm.GetTmStr(), TSecTm::GetCurTm().GetTmStr().CStr());
-  return 0;
+    fprintf(F, "# NId\tCommunityId\n");
+    for (int64 c = 0; c < CmtyV.Len(); c++) {
+        for (int64 i = 0; i < CmtyV[c].Len(); i++) {
+            fprintf(F, "%s\t%s\n",
+                    TInt64::GetStr(CmtyV[c][i].Val).CStr(),
+                    TInt64::GetStr(c).CStr());
+        }
+    }
+    fclose(F);
+
+    Catch
+            printf("\nrun time: %s (%s)\n", ExeTm.GetTmStr(), TSecTm::GetCurTm().GetTmStr().CStr());
+    return 0;
 }
