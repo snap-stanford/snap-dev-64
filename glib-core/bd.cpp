b8d75c4abd5dfe9f8b8ca4752e2786ba6b0e3fd9
Farzaan Kaiyom <39839866+farzaank@users.noreply.github.com>
2019-07-15 00:54:26 -0700

    added true support for GNU>=6
    removed quick fix to GNU error that defined struct exception temporarily

--- glib-core/bd.cpp
+++ glib-core/bd.cpp
@@ -15,19 +15,14 @@ int absolute(int n){
 }
 /////////////////////////////////////////////////
 // Mathmatical-Errors
-#if defined(__BCPLUSPLUS__) && (__BCPLUSPLUS__==0x0530)
+#if defined(__GNUC__) && (__GNUC__ >= 6)
+// skips matherr handling as matherr not supported in g++ 6 or later
+#elif defined(__BCPLUSPLUS__) && (__BCPLUSPLUS__==0x0530)
 int std::_matherr(struct math_exception* e){
   e->retval=0;
   return 1;
 }
 #elif defined(GLib_GLIBC) || defined(GLib_BSD)
-struct __exception {
-    int    type;      /* Exception type */
-    char*  name;      /* Name of function causing exception */
-    double arg1;      /* 1st argument to function */
-    double arg2;      /* 2nd argument to function */
-    double retval;    /* Function return value */
-};
 int _matherr(struct __exception* e){
   e->retval=0;
   return 1;
